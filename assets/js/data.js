bd8936c543f061927fbfca8ef6d3ebada27f78d4
alpaca <zxcvbnm5288@gmail.com>
2022-01-14 20:44:57 +0900

    add result article

--- assets/js/data.js
+++ assets/js/data.js
@@ -14,52 +14,29 @@ const QNA = [
         text: "답변입니다 3",
         score: 1,
       },
-      {
-        text: "답변입니다 4",
-        score: 1,
-      },
-    ],
-  },
-  {
-    q: "질문입니다 2",
-    a: [
-      {
-        text: "답변입니다 2-1",
-        score: 1,
-      },
-      {
-        text: "답변입니다 2-2",
-        score: 3,
-      },
-      {
-        text: "답변입니다 2-3",
-        score: 1,
-      },
-      {
-        text: "답변입니다 2-4",
-        score: 1,
-      },
-    ],
-  },
-  {
-    q: "질문입니다 3",
-    a: [
-      {
-        text: "답변입니다 3-1",
-        score: 1,
-      },
-      {
-        text: "답변입니다 3-2",
-        score: 3,
-      },
-      {
-        text: "답변입니다 3-3",
-        score: 1,
-      },
-      {
-        text: "답변입니다 3-4",
-        score: 1,
-      },
     ],
   },
 ];
+
+const RESULTS = {
+  1: {
+    img: "assets/img/arcane-caitlyn.png",
+    name: "Caitlyn",
+    description: "설명1",
+  },
+  2: {
+    img: "qopkdqop",
+    name: "이름2",
+    description: "설명2",
+  },
+  3: {
+    img: "qopkdqop",
+    name: "이름3",
+    description: "설명3",
+  },
+  4: {
+    img: "qopkdqop",
+    name: "이름4",
+    description: "설명4",
+  },
+};
--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,5 @@
 let score = 0;
+let share = ``;
 
 const isHide = (id) => {
   const target = document.querySelector(`#${id}`);
@@ -9,31 +10,71 @@ const isHide = (id) => {
     : target.classList.add("hide");
 };
 
+const removeChildren = (parentNode) => {
+  while (parentNode.childElementCount) {
+    parentNode.removeChild(parentNode.querySelector("li"));
+  }
+};
+
+const printResult = (value) => {
+  const result = document.querySelector("#result");
+  const img = result.querySelector(".result__img");
+  const name = result.querySelector(".result__name");
+  const description = result.querySelector(".result__description");
+  const shareKakaoBtn = result.querySelector(".result__share-kakao");
+
+  img.style.backgroundImage = `url(${RESULTS[value].img})`;
+  name.innerText = RESULTS[value].name;
+  description.innerText = RESULTS[value].description;
+};
+
 const printQna = (questionNumber) => {
+  if (questionNumber > QNA.length) {
+    isHide("qna");
+    isHide("result");
+    printResult(score);
+
+    return;
+  }
+
   const qna = document.querySelector("#qna");
   const statusBar = qna.querySelector(".status-bar");
   const question = qna.querySelector(".question");
-  const answers = qna.querySelectorAll(".answer");
+  const answersContainer = qna.querySelector(".answers-container");
+
+  removeChildren(answersContainer);
 
   statusBar.style.width = `${(100 * questionNumber) / QNA.length}%`;
   question.innerText = QNA[questionNumber - 1].q;
-  Array.from(answers).map((answer, i) => {
-    answer.innerText = QNA[questionNumber - 1].a[i].text;
-    answer.addEventListener("click", () => {
+
+  QNA[questionNumber - 1].a.map((answer, i) => {
+    const li = document.createElement("li");
+    li.classList.add("answer");
+    li.innerText = answer.text;
+
+    li.addEventListener("click", () => {
       score += QNA[questionNumber - 1].a[i].score;
       printQna(++questionNumber);
     });
+
+    answersContainer.appendChild(li);
   });
 };
 
 const init = () => {
+  /*
+  get query string
+  const urlSearchParams = new URLSearchParams(window.location.search);
+  const params = Object.fromEntries(urlSearchParams.entries());
+  */
+
   const start = document.querySelector(".start");
 
   start.addEventListener("click", () => {
     isHide("main");
     isHide("qna");
+    printQna(1);
   });
-  printQna(1);
 };
 
 init();

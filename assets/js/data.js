2fddf704ce66c34c6614fe27e618515efebd8a80
alpaca <zxcvbnm5288@gmail.com>
2022-02-07 23:32:22 +0900

    udpate data.js

--- assets/js/data.js
+++ assets/js/data.js
@@ -1,81 +1,145 @@
 const QNA = [
   {
-    question: "질문입니다 1",
+    question: "처음 하는 일에 나는?",
     answers: [
       {
-        text: "답변입니다 1",
-        score: 1,
+        text: "능숙하게 처음부터 잘한다.",
+        score: 9,
       },
       {
-        text: "답변입니다 2",
-        score: 3,
+        text: "몇 번 해보면 금방 잘하게 되는 편이다.",
+        score: 8,
       },
       {
-        text: "답변입니다 3",
+        text: "익숙해지는데 시간이 좀 걸리지만 잘한다.",
+        score: 7,
+      },
+      {
+        text: "남들보다 적응하는데 시간이 좀 걸리는 편이다.",
+        score: 5,
+      },
+    ],
+  },
+  {
+    question: "누군가 골목에서 위협을 받는다면?",
+    answers: [
+      {
+        text: "경찰에 신고하고 출동까지 지켜본다.",
+        score: 6,
+      },
+      {
+        text: "세상이 흉흉하다며 몰래 찍어 유튜브에 올린다.",
+        score: 2,
+      },
+      {
+        text: "나도 피해볼 수 있다. 관여하지 않는다.",
         score: 1,
       },
     ],
   },
   {
-    question: "질문입니다 222221",
+    question: "확고한 목표가 있나요?",
     answers: [
       {
-        text: "답변입니다 1231",
-        score: 1,
+        text: "뚜렷한 목표가 있다.",
+        score: 10,
       },
       {
-        text: "답변입니다 1232",
+        text: "뚜렷하진 않지만 목표가 있다.",
+        score: 8,
+      },
+      {
+        text: "새해 목표같은 가벼운 목표가 있다.",
+        score: 7,
+      },
+      {
+        text: "목표대신 매일 본능에 충실하게 살고있다.",
+        score: 2,
+      },
+    ],
+  },
+  {
+    question: "누군가 당신에게 못생겼다고 한다면?",
+    answers: [
+      {
+        text: "알게뭐람? 나는 내 외모가 맘에든다고 말해준다.",
+        score: 7,
+      },
+      {
+        text: "그렇게 생각할 수도 있지하고 넘어간다.",
+        score: 5,
+      },
+      {
+        text: "기분이 살짝 나쁘다.",
         score: 3,
       },
       {
-        text: "답변입니다 3124",
-        score: 1,
-      },
-      {
-        text: "답변입니다 3124",
+        text: "기분나쁘다. 너도 못생겼다고 말해준다.",
         score: 1,
       },
     ],
   },
   {
-    question: "질문입니다 333331",
+    question: "야근을 하고 퇴근길에 힘들일이 있다는 친구가 술마시자 하면?",
     answers: [
       {
-        text: "답변입니다 45121",
-        score: 1,
+        text: "당장만나자! 어디로 가야되냐고 물어본다.",
+        score: 10,
       },
       {
-        text: "답변입니다 2123",
+        text: "늦게까진 못마셔😅 적당히 얘기를 듣고 집으로 간다.",
+        score: 7,
+      },
+      {
+        text: "미안하다말하고 곧장 집으로 간다.",
+        score: 2,
+      },
+    ],
+  },
+  {
+    question: "조별과제의 리더가 되었다면?",
+    answers: [
+      {
+        text: "역할을 잘 배분하고 팀원을 다독여 잘 마무리한다.",
+        score: 9,
+      },
+      {
+        text: "사다리 타기로 역할을 정한다.",
+        score: 6,
+      },
+      {
+        text: "참여안한 팀원의 이름을 빼버린다.",
+        score: 1,
+      },
+    ],
+  },
+  {
+    question: "길에서 전단지를 나눠주던 사람이 전단지를 건넨다면?",
+    answers: [
+      {
+        text: "대꾸도 하지 않는다.",
+        score: 10,
+      },
+      {
+        text: "양손으로 가볍게 거절의사를 표시한다.",
+        score: 6,
+      },
+      {
+        text: "괜찮다고 하고 가던길을 간다.",
+        score: 5,
+      },
+      {
+        text: "거절을 못하겠다. 어차피 버릴거지만 일단 받는다.",
         score: 3,
-      },
-      {
-        text: "답변입니다 12533",
-        score: 1,
       },
     ],
   },
 ];
 
 const RESULTS = {
-  caitlyn: {
-    img: "assets/img/arcane-caitlyn.png",
-    name: "caitlyn",
-    description:
-      "케이틀린입니다 케이틀린입니다 케이틀린입니다 케이틀린입니다 케이틀린입니다 케이틀린입니다 케이틀린입니다 케이틀린입니다 케이틀린입니다 케이틀린입니다 케이틀린입니다 케이틀린입니다",
-  },
-  ekko: {
-    img: "assets/img/arcane-ekko.png",
-    name: "ekko",
-    description: "",
-  },
   jayce: {
     img: "assets/img/arcane-jayce.png",
     name: "jayce",
-    description: "",
-  },
-  mel: {
-    img: "assets/img/arcane-mel.png",
-    name: "mel",
     description: "",
   },
   powder: {
@@ -88,19 +152,9 @@ const RESULTS = {
     name: "silco",
     description: "",
   },
-  vander: {
-    img: "assets/img/arcane-vander.png",
-    name: "vander",
-    description: "",
-  },
   viktor: {
     img: "assets/img/arcane-viktor.png",
     name: "viktor",
-    description: "",
-  },
-  9: {
-    img: "assets/img/arcane-violet.png",
-    name: "violet",
     description: "",
   },
 };
